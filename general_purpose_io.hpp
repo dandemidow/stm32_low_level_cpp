f490594b734c4c9bcc9d3684f52f0260082863db
Danila Demidov <dandemidow@gmail.com>
2020-11-12 19:36:36 +0300

    handle alternate

--- general_purpose_io.hpp
+++ general_purpose_io.hpp
@@ -30,15 +30,20 @@ constexpr uint32_t kModerMode0  = Flag<0x3u, 0u>::value;
 constexpr uint32_t kModerMode00 = Flag<0x1u, 0u>::value;
 constexpr uint32_t kModerMode01 = Flag<0x2u, 0u>::value;
 
-constexpr uint32_t kOspeedrOspeed0 = Flag<0x3u, 0u>::value;
+constexpr uint32_t kOspeedrOspeed0  = Flag<0x3u, 0u>::value;
+constexpr uint32_t kOspeedrOspeed00 = Flag<0x1u, 0u>::value;
+constexpr uint32_t kOspeedrOspeed01 = Flag<0x2u, 0u>::value;
 
-constexpr uint32_t kPupdrPupd0 = Flag<0x3u, 0u>::value;
+constexpr uint32_t kPupdrPupd0  = Flag<0x3u, 0u>::value;
+constexpr uint32_t kPupdrPupd00 = Flag<0x1u, 0u>::value;
+constexpr uint32_t kPupdrPupd01 = Flag<0x2u, 0u>::value;
 
 constexpr uint32_t kAfrlAfsel0 = Flag<0xfu, 0u>::value;
 
 constexpr uint32_t kAfrlAfsel8 = Flag<0xfu, 0u>::value;
 
 constexpr uint32_t kPin5 = Flag<0x1u, 5u>::value;
+constexpr uint32_t kPin8 = Flag<0x1u, 8u>::value;
 }  // namespace flash
 
 using GeneralPurposeIO = Module<gpio::port,
--- ll_gpio.hpp
+++ ll_gpio.hpp
@@ -14,6 +14,19 @@ enum class gpio_mode {
   Analog = gpio::kModerMode0
 };
 
+enum class gpio_speed {
+  Low = 0x00,
+  Medium = gpio::kOspeedrOspeed00,
+  High = gpio::kOspeedrOspeed01,
+  VeryHigh = gpio::kOspeedrOspeed0
+};
+
+enum class gpio_pull {
+  No = 0x00,
+  Up = gpio::kPupdrPupd00,
+  Down = gpio::kPupdrPupd01
+};
+
 static inline void gpio_reset_output_pin(gpio::Pin &pin) {
   auto &port = pin.port();
   port.set<gpio::BRR>(pin.value());
@@ -27,20 +40,20 @@ static inline void gpio_set_pin_mode(gpio::Pin &pin, gpio_mode mode) {
               (static_cast<uint32_t>(mode) << position_pin));
 }
 
-static inline void gpio_set_pin_speed(gpio::Pin &pin, uint32_t  speed) {
+static inline void gpio_set_pin_speed(gpio::Pin &pin, gpio_speed  speed) {
   const uint32_t position_pos = pin.position() * 2u;
   auto &port = pin.port();
   reg::modify(port.get<gpio::OSPEEDR>(),
               (gpio::kOspeedrOspeed0 << position_pos),
-              (speed << position_pos));
+              (static_cast<uint32_t>(speed) << position_pos));
 }
 
-static inline void gpio_set_pin_pull(gpio::Pin &pin, uint32_t pull) {
+static inline void gpio_set_pin_pull(gpio::Pin &pin, gpio_pull pull) {
   const uint32_t position_pos = pin.position() * 2u;
   auto &port = pin.port();
   reg::modify(port.get<gpio::PUPDR>(),
               (gpio::kPupdrPupd0 << position_pos),
-              (pull << position_pos));
+              (static_cast<uint32_t>(pull) << position_pos));
 }
 
 static inline void gpio_set_af_pin_0_7(gpio::Pin &pin, uint32_t alternate) {
@@ -69,21 +82,15 @@ static inline void gpio_set_pin_output_type(gpio::Pin &pin, uint32_t pin_mask, u
 
 struct GPIOInitType {
   uint32_t Pin;
-  gpio_mode Mode;
-  uint32_t Speed;
+  gpio_mode  Mode;
+  gpio_speed Speed;
   uint32_t OutputType;
-  uint32_t Pull;
+  gpio_pull  Pull;
   uint32_t Alternate;
 };
 
 static bool gpio_init(gpio::Pin &pin, GPIOInitType *GPIO_InitStruct) {
   const uint32_t pinpos = pin.position();
-  uint32_t currentpin;
-
-  /* Check the parameters */
-  assert_param(IS_LL_GPIO_PIN(GPIO_InitStruct->Pin));
-  assert_param(IS_LL_GPIO_PULL(GPIO_InitStruct->Pull));
-
   auto &port = pin.port();
 
   /* Pin Mode configuration */
@@ -91,29 +98,19 @@ static bool gpio_init(gpio::Pin &pin, GPIOInitType *GPIO_InitStruct) {
 
   if ((GPIO_InitStruct->Mode == gpio_mode::Output) ||
       (GPIO_InitStruct->Mode == gpio_mode::Analog)) {
-        /* Check Speed mode parameters */
-        assert_param(IS_LL_GPIO_SPEED(GPIO_InitStruct->Speed));
-
         /* Speed mode configuration */
         gpio_set_pin_speed(pin, GPIO_InitStruct->Speed);
       }
 
       /* Pull-up Pull down resistor configuration*/
-      LL_GPIO_SetPinPull(GPIOx, currentpin, GPIO_InitStruct->Pull);
+      gpio_set_pin_pull(pin, GPIO_InitStruct->Pull);
 
-      if (GPIO_InitStruct->Mode == LL_GPIO_MODE_ALTERNATE)
-      {
-        /* Check Alternate parameter */
-        assert_param(IS_LL_GPIO_ALTERNATE(GPIO_InitStruct->Alternate));
-
+      if (GPIO_InitStruct->Mode == gpio_mode::Alternate) {
         /* Speed mode configuration */
-        if (currentpin < LL_GPIO_PIN_8)
-        {
-          LL_GPIO_SetAFPin_0_7(GPIOx, currentpin, GPIO_InitStruct->Alternate);
-        }
-        else
-        {
-          LL_GPIO_SetAFPin_8_15(GPIOx, currentpin, GPIO_InitStruct->Alternate);
+        if (pin.value() < gpio::kPin8) {
+          gpio_set_af_pin_0_7(pin, GPIO_InitStruct->Alternate);
+        } else {
+          gpio_set_af_pin_8_15(pin, GPIO_InitStruct->Alternate);
         }
       }
 
