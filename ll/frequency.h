117d78750a30f97804e21b6312ec7d839b0bcf2d
Danila Demidov <dandemidow@gmail.com>
2020-12-10 20:29:09 +0300

    add freq file

---
+++ ll/frequency.h
@@ -0,0 +1,39 @@
+#if !defined(LL_FRECUENCY_H_)
+#define LL_UTILS_H_
+
+#include <ratio>
+
+template <class Ratio>
+class Frequency {
+ private:
+  double value {0.0};
+ public:
+  typedef Ratio ratio;
+
+  Frequency() = default;
+  Frequency(const double& v) : value(v) {}
+
+  template <class R>
+  Frequency(const Frequency<R>& d)
+      : value(d.count() * std::ratio_divide<Ratio, R>::type::den /
+                          std::ratio_divide<Ratio, R>::type::num) {}
+
+  double count() const {return value;}
+
+  Frequency operator+(const Frequency& d) {return {value + d.count()};}
+  Frequency operator-(const Frequency& d) {return {value - d.count()};}
+
+  Frequency& operator+=(const Frequency& d) {value += d.count(); return *this;}
+  Frequency& operator-=(const Frequency& d) {value -= d.count(); return *this;}
+
+  Frequency operator+() const {return *this;}
+  Frequency operator-() const {return Frequency(-value);}
+
+  Frequency& operator*=(double rhs) {value *= rhs; return *this;}
+  Frequency& operator/=(double rhs) {value /= rhs; return *this;}
+};
+
+using hertz = Frequency<std::ratio<1>>;
+using kilohertz = Frequency<std::kilo>;
+
+#endif
