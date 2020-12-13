f2a894f45776e354b1cbaeb9e06b6046378cd9fa
Danila Demidov <dandemidow@gmail.com>
2020-12-13 20:04:30 +0300

    replace double by int

--- ll/frequency.h
+++ ll/frequency.h
@@ -6,20 +6,19 @@
 template <class Ratio>
 class Frequency {
  private:
-  double value {0.0};
+  uint32_t value {0u};
  public:
   using ratio = Ratio;
 
   constexpr Frequency() = default;
-  constexpr explicit Frequency(double v) : value{v} {}
-  constexpr explicit Frequency(uint32_t v) : value{static_cast<double>(v)} {}
+  constexpr explicit Frequency(uint32_t v) : value{v} {}
 
   template <class R>
   constexpr Frequency(const Frequency<R>& d)
       : value(d.count() * std::ratio_divide<Ratio, R>::type::den /
                           std::ratio_divide<Ratio, R>::type::num) {}
 
-  constexpr double count() const {return value;}
+  constexpr uint32_t count() const {return value;}
 
   constexpr Frequency operator+(const Frequency& d) const {return Frequency{value + d.count()};}
   constexpr Frequency operator-(const Frequency& d) const {return Frequency{value - d.count()};}
@@ -33,36 +32,24 @@ class Frequency {
   constexpr Frequency operator+() const {return *this;}
   constexpr Frequency operator-() const {return Frequency(-value);}
 
-  constexpr Frequency& operator*=(double rhs) {value *= rhs; return *this;}
-  constexpr Frequency& operator/=(double rhs) {value /= rhs; return *this;}
+  constexpr Frequency& operator*=(uint32_t rhs) {value *= rhs; return *this;}
+  constexpr Frequency& operator/=(uint32_t rhs) {value /= rhs; return *this;}
 };
 
 using hertz = Frequency<std::ratio<1>>;
 using kilohertz = Frequency<std::kilo>;
 using megahertz = Frequency<std::mega>;
 
-constexpr auto operator""_Hz ( long double freq ) {
-  return hertz{static_cast<double>(freq)};
-}
-
 constexpr hertz operator""_Hz ( unsigned long long int freq ) {
-  return hertz{static_cast<double>(freq)};
-}
-
-constexpr kilohertz operator""_KHz ( long double freq ) {
-  return kilohertz{static_cast<double>(freq)};
+  return hertz{static_cast<uint32_t>(freq)};
 }
 
 constexpr kilohertz operator""_KHz ( unsigned long long int freq ) {
-  return kilohertz{static_cast<double>(freq)};
-}
-
-constexpr megahertz operator""_MHz ( long double freq ) {
-  return megahertz{static_cast<double>(freq)};
+  return kilohertz{static_cast<uint32_t>(freq)};
 }
 
 constexpr megahertz operator""_MHz ( unsigned long long int freq ) {
-  return megahertz{static_cast<double>(freq)};
+  return megahertz{static_cast<uint32_t>(freq)};
 }
 
 #endif
