9fc7de84acf82f3faef34d494241fa20d4199aab
Danila Demidov <dandemidow@gmail.com>
2020-12-24 19:06:20 +0300

    fill output file

--- ll/gpio/gpio.cpp
+++ ll/gpio/gpio.cpp
@@ -35,15 +35,5 @@ void Pin::set_mode(mode mode) {
               (static_cast<uint32_t>(mode) << position_pin));
 }
 
-bool Output::init(const output &out_type,
-                  const pull &pull,
-                  const speed &speed) {
-    set_mode(mode::Output);
-    set_speed(speed);
-    set_pull(pull);
-    set_output_type(value_, out_type);
-    return true;
-}
-
 }  // namespace ll::gpio
 
