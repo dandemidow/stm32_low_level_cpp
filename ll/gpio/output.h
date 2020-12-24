f65076cc5a1e60593a8c6ef39e49c2ab4c3e206c
Danila Demidov <dandemidow@gmail.com>
2020-12-24 19:01:29 +0300

    split gpio file

---
+++ ll/gpio/output.h
@@ -0,0 +1,26 @@
+#if !defined(LL_GPIO_OUTPUT_H_)
+#define LL_GPIO_OUTPUT_H_
+
+#include "gpio.h"
+
+namespace ll::gpio {
+
+struct Output : public Pin {
+  Output(port p, uint32_t number) : Pin{p, number} {
+    switch (p) {
+    case port::A: bus::Grp1EnableClock(bus::ahb2::kGrp1PeriphGpioA);
+    }
+    reset();
+  }
+  inline void reset() {
+    gpio_.set<BRR>(value_);
+  }
+
+  bool init(const gpio::output &out_type,
+            const gpio::pull &pull,
+            const gpio::speed &speed);
+};
+
+}  // namespace ll::gpio
+
+#endif
