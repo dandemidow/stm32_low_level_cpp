f57f1b053a693a3b1a0d1729c8aaf21e9a993e1e
Danila Demidov <dandemidow@gmail.com>
2021-01-01 22:49:18 +0300

    hide platform deps details

--- ll/bus.hpp
+++ ll/bus.hpp
@@ -7,29 +7,6 @@
 
 namespace ll::bus {
 
-template <typename Bus>
-struct BusValue {
-  static constexpr uint32_t kBus = Bus::value;
-  uint32_t value;
-  operator uint32_t() const {
-    return value;
-  }
-};
-
-namespace apb1 {
-constexpr auto kGrp1PeriphPwr = rcc::kApb1Enr1PwrEn;
-}  // namespace apb1
-
-namespace apb2 {
-constexpr auto kGrp1PeriphSysCfg = rcc::kApb2EnrSysCfgEn;
-constexpr auto kGrp1PeriphAll = rcc::kApb2PeriphAll;
-}  // namespace apb2
-
-namespace ahb2 {
-using tag = std::integral_constant<uint32_t, rcc::AHB2ENR>;
-constexpr auto kGrp1PeriphGpioA = BusValue<tag>{address::ahb2::kGrp1PeriphGpioA};
-}  // namespace ahb2
-
 template <uint32_t Reg>
 static inline void GrpEnableClock(const RegisterValue<Reg> &periphs) {
   auto &rcc = *new ResetClockControl{};
