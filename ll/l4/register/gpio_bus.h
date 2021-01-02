68284a3344f634f620e792a799bb39c78f0982cd
Danila Demidov <dandemidow@gmail.com>
2021-01-02 09:14:05 +0300

    make it inline to avoid multiple definitions

--- ll/l4/register/gpio_bus.h
+++ ll/l4/register/gpio_bus.h
@@ -1,12 +1,12 @@
-#if !defined(GPIO_BUS_HPP_)
-#define GPIO_BUS_HPP_
+#if !defined(GPIO_BUS_H_)
+#define GPIO_BUS_H_
 
 #include "general_purpose_io.hpp"
 #include "bus.hpp"
 
 namespace ll::gpio {
 
-auto EnablePort(const port &p) {
+inline auto EnablePort(const port &p) {
   switch (p) {
   case port::A: bus::GrpEnableClock(rcc::kGrp1PeriphGpioA);
   }
