c678c85a334f371d426f394ae154bc4e43f0a299
Danila Demidov <dandemidow@gmail.com>
2021-01-02 13:43:57 +0300

    add cm0 file

---
+++ ll/core/m0/cm0.hpp
@@ -0,0 +1,39 @@
+#if !defined(CORE_CM0_HPP_)
+#define CORE_CM0_HPP_
+
+#include "register/nested_vectored_interrupt_controller.hpp"
+#include "register/system_control_block.h"
+#include "irqs.h"
+
+namespace ll::nvic {
+
+using address::nvic::PriorityGroup;
+
+constexpr uint32_t BitShift(IRQn_Type irq) {
+  return (static_cast<uint32_t>(irq) & 0b11) * 8u;
+}
+
+constexpr uint32_t ShpIndex(IRQn_Type irq) {
+  return ((static_cast<uint32_t>(irq) & 0b1111) - 8ul) >> 2ul;
+}
+
+constexpr uint32_t IpIndex(IRQn_Type irq) {
+  return static_cast<uint32_t>(irq) >> 2ul;
+}
+
+static inline void set_priority(IRQn_Type IRQn, uint32_t priority = 0u) {
+  using address::nvic::kPrioBits;
+  auto value = ((priority << (8U - kPrioBits)) & (uint32_t)0xfful) << BitShift(IRQn);
+  if (static_cast<int>(IRQn) >= 0) {
+    auto &nvic = *new NestedVectoredInterruptController{};
+    const uint32_t irq_index = IpIndex(IRQn);
+    nvic.get<nvic::IP>()[irq_index] = (nvic.get<nvic::IP>()[irq_index] & ~(0xfful << BitShift(IRQn))) | value;
+  } else {
+    auto &scb = *new SystemControlBlock{};
+    const uint32_t irq_index = ShpIndex(IRQn);
+    scb.get<scb::SHP>()[irq_index] = (scb.get<scb::SHP>()[irq_index] & ~(0xfful << BitShift(IRQn))) | value;
+  }
+}
+
+}  // namespace ll::nvic
+#endif
