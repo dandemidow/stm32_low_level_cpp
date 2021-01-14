2e18205f5ba97489badbb48111ce06094e63557d
Danila Demidov <dandemidow@gmail.com>
2021-01-14 20:29:35 +0300

    add missed file:

---
+++ ll/f0/register/tim.hpp
@@ -0,0 +1,66 @@
+#if !defined(TIM_HPP_)
+#define TIM_HPP_
+
+#include "addresses.h"
+#include "device_register.h"
+#include "module.h"
+
+namespace ll::tim {
+enum class index : uint32_t {
+  T1 = address::tim::kT1,
+  T3 = address::tim::kT3
+};
+
+enum : uint32_t {
+  CR1,
+  CR2,
+  SMCR,
+  DIER,
+  SR,
+  EGR,
+  CCMR1,
+  CCMR2,
+  CCER,
+  CNT,
+  PSC,
+  ARR,
+  RCR,
+  CCR1,
+  CCR2,
+  CCR3,
+  CCR4,
+  BDTR,
+  DCR,
+  DMAR,
+  OR
+};
+
+}  // namespace ll::tim
+
+using Tim = Module<ll::tim::index,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register,
+Register
+>;
+
+static_assert(std::is_standard_layout<Tim>::value);
+
+#endif
